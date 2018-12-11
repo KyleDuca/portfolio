ae6ced8c691e65f1b57aa1acee7a99e217278938
Kyle Duca <kyleduca@gmail.com>
2018-12-11 07:36:57 +0000

    Set up Edit, New, and Show pages and the controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,46 @@
 class PostsController < ApplicationController
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
+  
   def index
   end
+
+  def new
+    @post = Post.new
+  end 
+
+  def create
+    @post = Post.new post_params
+
+    if @post.save
+      redirect_to @post, notice: "Remember the benevolent universe premise."
+    else
+      render 'new', notice: "Did not save post."
+    end
+  end
+
+  def show
+    find_post
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update post_params
+      redirect_to @post, notice: "If success was impossible, then no invention, not even fire, would exist."
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+
+  def find_post
+    @post = Post.find(params[:id])
+  end
+
 end
